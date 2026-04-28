afdab1c84e560e4e8cb6473ba94e648071ad43a5
amychou1266-debug <amychou1266@gmail.com>
2026-04-28 11:48:05 +0800

    Update index.py

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 import requests
 from bs4 import BeautifulSoup
 
@@ -43,6 +45,7 @@ def index():
     homepage += "<a href='/search'>老師查詢</a><br>"
     homepage += "<a href='/spider1'>爬蟲</a><br>"
     homepage += "<a href='/movie'>查詢即將上映電影</a><br>"
+    homepage += "<br><a href=/movie2>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
     return homepage
 
 # =========================
@@ -206,6 +209,41 @@ def movie():
         html += f"<a href='{link}' target='_blank'>{name}</a><br>"
 
     return html
+
+@app.route("/movie2")
+def movie2():
+  url = "http://www.atmovies.com.tw/movie/next/"
+  Data = requests.get(url)
+  Data.encoding = "utf-8"
+  sp = BeautifulSoup(Data.text, "html.parser")
+  result=sp.select(".filmListAllX li")
+  lastUpdate = sp.find("div", class_="smaller09").text[5:]
+
+  for item in result:
+    picture = item.find("img").get("src").replace(" ", "")
+    title = item.find("div", class_="filmtitle").text
+    movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+    hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+    show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+    show = show.replace("片長：", "")
+    show = show.replace("分", "")
+    showDate = show[0:10]
+    showLength = show[13:]
+
+    doc = {
+        "title": title,
+        "picture": picture,
+        "hyperlink": hyperlink,
+        "showDate": showDate,
+        "showLength": showLength,
+        "lastUpdate": lastUpdate
+      }
+
+    db = firestore.client()
+    doc_ref = db.collection("電影").document(movie_id)
+    doc_ref.set(doc)    
+  return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
+
 
 # =========================
 # 主程式
