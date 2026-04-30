f8622de81559da6b8eeb5a0c098a1f301efd5828
amychou1266-debug <amychou1266@gmail.com>
2026-04-30 21:16:40 +0800

    Update movie2.py

--- spider/movie2.py
+++ spider/movie2.py
@@ -1,87 +1,40 @@
-from bs4 import BeautifulSoup
-import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
+cred = credentials.Certificate("../serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
-# Firebase 初始化
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-key_path = os.path.join(BASE_DIR, "serviceAccountKey.json")
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate(key_path)
-    firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-# 爬蟲網址
+import requests
+from bs4 import BeautifulSoup
 url = "http://www.atmovies.com.tw/movie/next/"
 Data = requests.get(url)
 Data.encoding = "utf-8"
-
 sp = BeautifulSoup(Data.text, "html.parser")
-result = sp.select(".filmListAllX li")
-
+result=sp.select(".filmListAllX li")
+lastUpdate = sp.find("div", class_="smaller09").text[5:]
 info = ""
-
 for item in result:
-    img_tag = item.find("img")
-    title_tag = item.find("div", class_="filmtitle")
-    runtime_tag = item.find("div", class_="runtime")
-    update_tag = item.find("div", class_="smaller09")
+  picture = item.find("img").get("src").replace(" ", "")
+  title = item.find("div", class_="filmtitle").text
+  movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+  hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+  show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+  show = show.replace("片長：", "")
+  show = show.replace("分", "")
+  showDate = show[0:10]
+  showLength = show[13:]
+  info += picture + "\n" + title + "\n" + hyperlink + "\n" + showDate + "\n" + showLength + "\n\n"
+  print(info)
 
-    if img_tag:
-        picture = img_tag.get("src").replace(" ", "")
-    else:
-        picture = "無圖片"
+  doc = {
+      "title": title,
+      "picture": picture,
+      "hyperlink": hyperlink,
+      "showDate": showDate,
+      "showLength": showLength,
+      "lastUpdate": lastUpdate
+  }
 
-    if title_tag:
-        title = title_tag.text.strip()
-        link_tag = title_tag.find("a")
-
-        if link_tag:
-            hyperlink = "http://www.atmovies.com.tw" + link_tag.get("href")
-        else:
-            hyperlink = "無連結"
-    else:
-        title = "無標題"
-        hyperlink = "無連結"
-
-    showDate = "無上映日期"
-    showLength = "尚無片長資訊"
-
-    if runtime_tag:
-        show = runtime_tag.text.strip()
-
-        if "上映日期：" in show:
-            showDate = show.replace("上映日期：", "")[0:10]
-
-        if "片長：" in show:
-            showLength = show.split("片長：")[1].replace("分", "").replace(" ", "")
-
-    if update_tag:
-        lastUpdate = update_tag.text.strip()
-    else:
-        lastUpdate = "無更新日期"
-
-    movie_id = title.replace("/", "").replace("\\", "").replace(" ", "")
-
-    doc = {
-        "title": title,
-        "picture": picture,
-        "hyperlink": hyperlink,
-        "showDate": showDate,
-        "showLength": showLength,
-        "lastUpdate": lastUpdate
-    }
-
-    db.collection("電影").document(movie_id).set(doc)
-
-    info += picture + "\n"
-    info += title + "\n"
-    info += hyperlink + "\n"
-    info += showDate + "\n"
-    info += showLength + "\n"
-    info += lastUpdate + "\n\n"
-
-print(info)
+  db = firestore.client()
+  doc_ref = db.collection("電影").document(movie_id)
+  doc_ref.set(doc)
