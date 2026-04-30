7f7c28db3f247bb014ee804f2be28194bb8bbcc4
amychou1266-debug <amychou1266@gmail.com>
2026-04-30 21:32:08 +0800

    Update movie2.py

--- spider/movie2.py
+++ spider/movie2.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate("../serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+
 
 
 import requests
