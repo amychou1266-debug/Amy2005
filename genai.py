365eb4491cab6b2cdc05a15abbd759ce1593b66e
amychou1266-debug <amychou1266@gmail.com>
2026-05-26 10:44:03 +0800

    Update genai.py

--- genai.py
+++ genai.py
@@ -1,13 +1,17 @@
 import os
 from google import genai
 
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
-Question = input('請問要問AI什麼問題?')
+# 讀取環境變數
+api_key = os.getenv("GEMINI_API_KEY")
+
+# 建立 Gemini Client
+client = genai.Client(api_key=api_key)
+
+Question = input("請問要問AI什麼問題？")
+
 response = client.models.generate_content(
-    model='gemini-3.5-flash',
-    contents=Question,
+    model="gemini-2.5-flash",
+    contents=Question
 )
 
 print(response.text)
